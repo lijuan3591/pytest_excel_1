e7e53628f27b19ddba093dd2dda7bce15a124031
lijuan <lijuan3@tiens.com>
2023-02-28 10:01:54 +0800

    修改相对路径

--- testcases/scenario_test/test_01_productlist.py
+++ testcases/scenario_test/test_01_productlist.py
@@ -10,24 +10,24 @@ from operation.user_operation import user_order_delivery, assert_validate, get_e
 
 # PATH=r'D:\pytest_excel_1\data\vshare_scenario.xlsx'
 BASE_PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
-PATH = os.path.join(BASE_PATH, "data", "vshare.xlsx")
+PATH1 = os.path.join(BASE_PATH, "data", "vshare.xlsx")
 
 
 def setup_module():
     global excel, sheet
-    excel = openpyxl.load_workbook(PATH)
+    excel = openpyxl.load_workbook(PATH1)
     sheet = excel['info']
 
 class TestProductlist:
 
-    @pytest.mark.parametrize('data',re.read_excel(PATH))
+    @pytest.mark.parametrize('data',re.read_excel(PATH1))
     def test_productlist(self,data):
         r = data[0] + 1
         rd.report_api(data[11],data[17],data[16],data[18],data[19])
         res = user_order_delivery(data[3],data[1],data[2],data[5],data[4],data[7],data[6],data[15])
-        assert_validate(excel,sheet,r,res,data[8],data[9],PATH)
+        assert_validate(excel,sheet,r,res,data[8],data[9],PATH1)
         get_extract_data(data[12], data[13], res, data[14])
-        check_user_order(sheet,excel,PATH,r,data[20],data[21])
+        check_user_order(sheet,excel,PATH1,r,data[20],data[21])
 
 if __name__ == '__main__':
     t=TestProductlist().test_productlist().text
